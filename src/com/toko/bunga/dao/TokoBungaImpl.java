fbd7d6d5eae220885f59f479305195e9fec4b5f5
mutiarakhatami <117352562+mutiarakhatami@users.noreply.github.com>
2022-12-05 08:38:37 +0700

    Add files via upload

--- src/com/toko/bunga/dao/TokoBungaImpl.java
+++ src/com/toko/bunga/dao/TokoBungaImpl.java
@@ -6,17 +6,20 @@
 package com.toko.bunga.dao;
 
 import com.toko.bunga.model.TokoBunga;
+import java.io.Serializable;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author ASUS
  */
 @Repository
+@Transactional
 public class TokoBungaImpl implements TokoBungaDao{
     @Autowired
     private SessionFactory sessionFactory;
@@ -28,9 +31,9 @@ public class TokoBungaImpl implements TokoBungaDao{
     
     
     @Override
-    public void save(TokoBunga tokobunga) {
-        sessionFactory.getCurrentSession().save(tokobunga);
-        
+    public String save(TokoBunga tokobunga) {
+       Serializable res = sessionFactory.getCurrentSession().save(tokobunga);
+        return String.valueOf(res);
 //        Session session = sessionFactory.openSession();
 //        try{
 //            session.beginTransaction();
